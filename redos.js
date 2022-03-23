cfb28d9bf635f56456249f7ace7499734b7b1da9
Shutch748 <99168912+Shutch748@users.noreply.github.com>
2022-03-22 19:59:16 -0700

    Create redos.md
    @Shutch748

--- redos.js
+++ redos.js
@@ -15,6 +15,4 @@ for(var i = 1; i <= 5000000; i++) {
         pathParse.posix(attack_str);
         pathParse.win32(attack_str);
         var time_cost = Date.now() - time;
-        console.log("attack_str.length: " + attack_str.length + ": " + time_cost+" ms")
-    }
-}
+        eedb4ab91a67329a236d7e6e14fc78c5c56ac22f14390353c9af68e83c99cca4
